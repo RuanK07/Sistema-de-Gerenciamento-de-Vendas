e266ca5e5e56512bee4f88eb21442d425bf91925
RuanK07 <ruank07@gmail.com>
2023-05-03 11:20:12 -0300

    Projeto criado

---
+++ src/test/java/com/ruankennedy/cursomc/ModelagemConceitualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruankennedy.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelagemConceitualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
