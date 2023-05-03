fa2006f2e10984d1810be06649da374740561d33
RuanK07 <ruank07@gmail.com>
2023-05-03 14:22:02 -0300

    Produto e associacao muitos para muitos

---
+++ src/main/java/com/ruankennedy/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,8 @@
+package com.ruankennedy.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ruankennedy.cursomc.domain.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+}
